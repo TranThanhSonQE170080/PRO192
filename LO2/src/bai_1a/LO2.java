35eb57c8d6fec810de7e33e3dd8fbaf087796294
TrannThanhSonQE170080 <gamingts271@gmail.com>
2023-10-03 16:51:14 +0700

    UPfile1

---
+++ LO2/src/bai_1a/LO2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package bai_1a;
+
+/**
+ *
+ * @author son
+ */
+public class LO2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
